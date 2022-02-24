ad125eeb672298bb4ec70033e4e693582ffb4e5c
Emma Bang Creutzberg <emma213r@edu.zealand.dk>
2022-02-24 20:10:54 +0100

    Add project files.

---
+++ Opgave1UnitTest/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Opgave1UnitTest
+{
+    public class Car
+    {
+        private string _model;
+        private int _price;
+        private string _licensePlate;
+
+        public int Id { get; set; }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                if (value.Length < 4) throw new ArgumentException(message: "Model name must be at least 4 characters");
+                _model = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException( "Price", value, "Lets make some moneeeyz");
+                _price = value;
+            }
+        }
+
+        public string LicensePlate
+        {
+            get => _licensePlate;
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(value));
+                if (value.Length < 2 || value.Length < 7) throw new ArgumentOutOfRangeException(nameof(value));
+                _licensePlate = value;
+            }
+        }
+
+        public Car(int id, string model, int price, string licensePlate)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+    }
+}
---
+++ Opgave1UnitTestTests/CarTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave1UnitTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1UnitTestTests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car _car;
+        [TestInitialize()]
+        public void SuperCarTest()
+        {
+            _car = new Car(id: 1, model: "Tesla", price: 10000, licensePlate: "AB23780");
+        }
+
+        [TestMethod()]
+        public void ModelTest()
+        {
+            Assert.AreEqual("Tesla", _car.Model);
+            Assert.ThrowsException<ArgumentException>(() => _car.Model = "BMW");
+            Assert.ThrowsException<ArgumentNullException>(() => _car.Model = null);
+        }
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual( 10000, _car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = -1);
+        }
+
+        [TestMethod()]
+        public void LicensePlateTest()
+        {
+            Assert.AreEqual( "AB23780",  _car.LicensePlate);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.LicensePlate = "he");
+            Assert.ThrowsException<ArgumentNullException>(() => _car.LicensePlate = null);
+        }
+    }
+}
